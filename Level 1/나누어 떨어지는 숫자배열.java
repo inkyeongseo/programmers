5b33d3b35878c0e3bab6206db4abcdecd175c934
inkyeong <dlsrud0415@naver.com>
2021-10-24 23:42:10 +0900

    나누어 떨어지는 숫자배열(JAVA)

---
+++ Level 1/나누어 떨어지는 숫자배열.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[] arr, int divisor) {
+        
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] % divisor == 0){
+                list.add(arr[i]);
+            } 
+        }
+        if(list.size() == 0){
+            list.add(-1);
+        }
+        int[] answer = new int[list.size()];
+        
+        for(int j = 0; j < list.size(); j++){
+            answer[j] = list.get(j);
+        }
+        Arrays.sort(answer);
+        return answer;
+    }
+}
