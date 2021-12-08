4281c713002975bdca2cdc7de12031cfd76a33a6
inkyeong <dlsrud0415@naver.com>
2021-12-08 21:53:12 +0900

    구명보트(JAVA)

---
+++ Level 2/구명보트.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] people, int limit) {
+        int answer = 0;
+        
+        Arrays.sort(people);
+        int j = 0;
+        for(int i = people.length-1; i >=j; i--){
+            if(people[i]+people[j] <= limit){
+                answer++;
+                j++;
+            }else{
+                answer++;
+            }
+        }
+        
+        
+        return answer;
+    }
+}
